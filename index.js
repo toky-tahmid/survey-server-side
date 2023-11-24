24409bec0dbf3d81a4a45ed23d6cdc1e3b115c8b
Your Name <you@example.com>
2023-11-25 01:33:26 +0600

    all surveys are posted

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khqul4z.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,12 +18,36 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const surveyCollection = client.db("survey").collection("allSurveys");
 
+    app.get('/allSurveys', async (req, res) => {
+        const result = await surveyCollection.find().toArray();
+        res.send(result);
+      });
+      app.post('/allSurveys', async (req, res) => {
+        const { title, description, category, options, likeDislike } = req.body;
+        const timestamp = new Date();
+    
+        const newSurvey = {
+          title,
+          description,
+          category,
+          options,
+          likeDislike,
+          like: 0,         // Initialize like to 0
+          dislike: 0,      // Initialize dislike to 0
+          timestamp,
+        };
+      
+        const result = await surveyCollection.insertOne(newSurvey);
+        res.json({ insertedId: result.insertedId });
+      });
+      
+  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
